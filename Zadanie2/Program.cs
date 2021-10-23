3b0a4903b2702f6accd9c848767de7e9f7b8ee4a
VladimirNikl <martinukvn@ukr.net>
2021-10-23 22:51:08 +0300

    Edit zadan2

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -10,9 +10,24 @@ namespace Zadanie2
     {
         static void Main()
         {
-            int i = 5;
-            MyList<string> strCollect = new MyList<string>("k","i");
-            foreach(string item in strCollect)
+            int i = 1;
+            bool flag = true;
+            string line = null;
+            Console.WriteLine("     Введем элементы коллекции:");
+            do
+            {
+                line += Console.ReadLine();
+                Console.WriteLine("    Закончить -  нажать '0', продолжить - иначе.");
+                string endInput = Console.ReadLine();
+                if (endInput == "0")
+                    flag = false;
+            } while (flag);
+            MyList<string> strCollect = new MyList<string>(line);
+            foreach (string item in strCollect)
+                Console.WriteLine(item);
+            Console.WriteLine(string.Format("+-+-"));
+            MyList<int> intCollect = new MyList<int>(284, 48);
+            foreach (int item in intCollect)
                 Console.WriteLine(item);
 
             Console.ReadKey();
